9006a1c90bc3ed38aa05e96602a5b29fba4ea52c
Avery <averygallagan@yahoo.com>
2024-03-18 16:53:22 -0400

    launch page html and css complete

---
+++ assets/js/form.js
@@ -0,0 +1,7 @@
+const Submit = document.querySelector('#submitbtn')
+
+Submit.addEventListener("submit", function(event) {
+    event.preventDefault();
+    window.location.href = "Avery-Gallagan-Personal-Blog/blog.html";
+});
+
